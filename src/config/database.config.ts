55230466a6fa2f9cc1d1089aca39f844d7d48cee
muhammed <eng.muhammedmurad@gmail.com>
2025-06-06 21:41:01 +0300

    Add DatabaseConfigModule for MongoDB connection

---
+++ src/config/database.config.ts
@@ -0,0 +1,21 @@
+// src/config/database.config.ts
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [MongooseModule],
+})
+export class DatabaseConfigModule {}
